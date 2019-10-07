4210cb8bc21a05f82b701cfae39cdca155129a71
Hasanboy <stainlessboy@gmail.com>
2019-10-07 18:22:17 +0500

    remove secret ket

--- config/settings_pro.py
+++ config/settings_pro.py
@@ -16,7 +16,7 @@ from .settings import *
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
+# SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
