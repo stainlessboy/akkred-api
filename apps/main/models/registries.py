fbb9c2b8e85110edd82c0e63a774856e2fe00134
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-11-07 13:31:12 +0500

    add new fields

--- apps/main/models/registries.py
+++ apps/main/models/registries.py
@@ -52,4 +52,4 @@ class Registries(models.Model):
     web_site_ao = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return self.id
+        return self.title_organ
