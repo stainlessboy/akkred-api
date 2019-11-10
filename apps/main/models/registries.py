bba273c04dd6e1acb1b2df5292e8619572e723c1
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-11-11 01:03:36 +0500

    change position

--- apps/main/models/registries.py
+++ apps/main/models/registries.py
@@ -15,41 +15,42 @@ class Registries(models.Model):
         (EXTENDED, 'extended'),
     )
 
-    title_organ = models.CharField(max_length=255)
     number = models.CharField(max_length=255, null=True)
-    code = models.CharField(max_length=255, null=True)
-    status = models.CharField(max_length=20, choices=STATUS_TYPES,
-                              default=INACTIVE)
-    full_name_supervisor_ao = models.CharField(max_length=255, null=True)
-    position_supervisor_legal = models.CharField(max_length=255, null=True)
-    address_applicant = models.CharField(max_length=1000, null=True)
-    title_applicant = models.CharField(max_length=1000, null=True)
-
-    type_organ = models.ForeignKey('main.TypeOrgan', PROTECT,
-                                   related_name='registry')
     inn = models.CharField(max_length=455, null=True)
+    title_applicant = models.CharField(max_length=1000, null=True)
+    form_ownership = models.CharField(max_length=455)
+    position_supervisor_legal = models.CharField(max_length=255, null=True)
+    full_name_supervisor_main = models.CharField(max_length=255, null=True)
+    address_applicant = models.CharField(max_length=1000, null=True)
     phone = models.CharField(max_length=255, null=True)
     email = models.CharField(max_length=255, null=True)
     web_site = models.CharField(max_length=255, null=True, blank=True)
+    type_organ = models.ForeignKey('main.TypeOrgan', PROTECT,
+                                   related_name='registry')
+    title_organ = models.CharField(max_length=255)
     address_organ = models.CharField(max_length=1000, null=True)
-    region = models.ForeignKey('main.Region', PROTECT, null=True, blank=True)
-    keywords = models.CharField(max_length=255)
-    text = models.TextField(null=True, blank=True)
-    area = models.CharField(max_length=255)
+    position_supervisor_ao = models.CharField(max_length=255, null=True)
+    full_name_supervisor_ao = models.CharField(max_length=255, null=True)
+    phone_ao = models.CharField(max_length=255, null=True)
+    email_ao = models.CharField(max_length=255, null=True)
+    web_site_ao = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_TYPES,
+                              default=INACTIVE)
     status_date = models.DateField(null=True)
     accreditation_date = models.DateField(null=True)
     accreditation_duration = models.DateField(null=True, blank=True)
     accreditation_duration_text = models.CharField(max_length=1000, null=True, blank=True)
-    form_ownership = models.CharField(max_length=455)
     designation_of_the_fundamental_standard = models.CharField(max_length=455, null=True)
+
+    code = models.CharField(max_length=255, null=True)
+
+    region = models.ForeignKey('main.Region', PROTECT, null=True, blank=True)
+    keywords = models.CharField(max_length=255)
+    text = models.TextField(null=True, blank=True)
+    area = models.CharField(max_length=255)
+
     file = models.ForeignKey('main.File', on_delete=CASCADE, null=True,
                              related_name='registers', blank=True)
-
-    full_name_supervisor_main = models.CharField(max_length=255, null=True)
-    position_supervisor_ao = models.CharField(max_length=255, null=True)
-    phone_ao = models.CharField(max_length=255, null=True)
-    email_ao = models.CharField(max_length=255, null=True)
-    web_site_ao = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.title_organ
