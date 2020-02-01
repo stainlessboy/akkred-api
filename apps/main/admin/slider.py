f87c8d8ff9a0fbefbeb2f83090dcaabe495a08f5
Hasanboy <stainlessclash@gmail.com>
2020-02-02 00:48:46 +0500

    add transaltion

--- apps/main/admin/slider.py
+++ apps/main/admin/slider.py
@@ -4,10 +4,11 @@ from main.models import Slider
 
 @admin.register(Slider)
 class Admin(admin.ModelAdmin):
-    list_display = ['title', 'id']
+    list_display = ['id', 'title']
     search_fields = ['title', 'title_uz', 'title_ru', 'title_en']
     fields = [
         'title_en',
         'title_ru',
-        'title_uz'
+        'title_uz',
+        'image'
     ]
