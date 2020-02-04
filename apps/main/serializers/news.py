f513cc3a5436c2f31b4585d537b52c62d8bf9d15
Hasanboy <stainlessclash@gmail.com>
2020-02-04 10:17:03 +0500

    fix

--- apps/main/serializers/news.py
+++ apps/main/serializers/news.py
@@ -4,6 +4,8 @@ from main.models import News
 
 
 class NewsModelSerializer(serializers.ModelSerializer):
+    created_date_by_admin = serializers.DateTimeField(format="%Y-%m-%d")
+
     class Meta:
         model = News
         fields = '__all__'
