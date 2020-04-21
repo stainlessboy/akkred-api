0653a887cbb7c66e5e5fe15358287eafc7fee87e
Hasanboy <stainlessclash@gmail.com>
2020-04-21 17:46:55 +0500

    add actions

--- apps/main/admin/code.py
+++ apps/main/admin/code.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
-from main.models import Code, CodeType
+from main.models import Code, CodeType, Registries
 
 from import_export.admin import ImportExportModelAdmin
 
 
+# def find_all_area(self, **_):
+#     registries = Registries.objects.all()
+#     for registrie in registries:
+#         res = Code.objects.filter(organ_number=registrie.area)
+#         if res.exists():
+#             registrie.code_nd.set(res)
+
+
+def export_csv(modeladmin, request, queryset):
+    registries = Registries.objects.all()
+    for registrie in registries:
+        res = Code.objects.filter(organ_number=registrie.area)
+        if res.exists():
+            registrie.code_nd.set(res)
+
+
+export_csv.short_description = u"Done all"
+
+
 @admin.register(Code)
 class Admin(ImportExportModelAdmin):
+    actions = [export_csv]
     list_display = ['cod_tnved', 'organ_number']
     search_fields = ['cod_tnved', 'organ_number']
