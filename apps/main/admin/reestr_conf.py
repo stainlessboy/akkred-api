d9efb5302ac41d3346eb1a0a8ac7bb3da7dbee23
Hasanboy <stainlessclash@gmail.com>
2020-08-30 00:29:43 +0500

    fix

--- apps/main/admin/reestr_conf.py
+++ apps/main/admin/reestr_conf.py
@@ -1,10 +1,84 @@
 from django.contrib import admin
+from django.http import HttpResponse
 
 from core.printable.akkred import AkkredPDF
 from core.rest_framework.printable_responses import pdf_response
 from main.models import ConfirmReestr, ConfirmReestrStatus
-from django.forms import TextInput, Textarea
+from django.forms import Textarea
 from django.db import models
+
+
+def export_csv(modeladmin, request, queryset):
+    import csv
+    from django.utils.encoding import smart_str
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=mymodel.csv'
+    writer = csv.writer(response, csv.excel)
+    response.write(u'\ufeff'.encode(
+        'utf8'))  # BOM (optional...Excel needs it to open UTF-8 file properly)
+    writer.writerow([
+        smart_str(u"ID"),
+        smart_str(u"Number"),
+        smart_str(u"Inn"),
+        smart_str(u"title_yurd_lisa"),
+        smart_str(u"title_organ"),
+        smart_str(u"title_organ_type"),
+        smart_str(u"address"),
+        smart_str(u"address_organ"),
+        smart_str(u"phone"),
+        smart_str(u"email"),
+        smart_str(u"web_site"),
+        smart_str(u"address_organ"),
+        smart_str(u"full_name_supervisor_ao"),
+        smart_str(u"phone_ao"),
+        smart_str(u"email_ao"),
+        smart_str(u"status"),
+        smart_str(u"status_date"),
+        smart_str(u"accreditation_date"),
+        smart_str(u"validity_date"),
+        smart_str(u"reissue_date"),
+        smart_str(u"designation_of_the_fundamental_standard"),
+        smart_str(u"oked"),
+        smart_str(u"soogu"),
+
+        smart_str(u"Branches"),
+    ])
+    for obj in queryset:
+        directions_list = []
+        for direction in obj.directions.all():
+            directions_list.append(direction.name)
+
+        writer.writerow([
+            smart_str(obj.pk),
+            smart_str(obj.number),
+            smart_str(obj.inn),
+            smart_str(obj.title_yurd_lisa),
+            smart_str(obj.title_organ),
+            smart_str(obj.title_organ_type),
+            smart_str(obj.address),
+            smart_str(obj.address_organ),
+            smart_str(obj.phone),
+            smart_str(obj.email),
+            smart_str(obj.web_site),
+            smart_str(obj.address_organ),
+            smart_str(obj.full_name_supervisor_ao),
+            smart_str(obj.phone_ao),
+            smart_str(obj.email_ao),
+            smart_str(obj.status),
+            smart_str(obj.status_date),
+            smart_str(obj.accreditation_date),
+            smart_str(obj.validity_date),
+            smart_str(obj.reissue_date),
+            smart_str(obj.designation_of_the_fundamental_standard),
+            smart_str(obj.oked),
+            smart_str(obj.soogu),
+
+            smart_str(directions_list),
+        ])
+    return response
+
+
+export_csv.short_description = u"Export CSV"
 
 
 class RegistriesStatusInline(admin.TabularInline):
@@ -18,6 +92,7 @@ class RegistriesStatusInline(admin.TabularInline):
 @admin.register(ConfirmReestr)
 class Admin(admin.ModelAdmin):
     list_display = ['title_organ']
+    actions = [export_csv]
     filter_horizontal = ('directions',)
     search_fields = ['number', 'title_organ', 'inn', 'id', 'code']
     list_filter = ['region', 'status', 'is_fact_address',
