b6d6f61964e845beec9003a6ac4394890b1d607c
Hasanboy <stainlessclash@gmail.com>
2020-09-03 00:59:28 +0500

    fix

--- apps/main/serializers/authorized_reestr.py
+++ apps/main/serializers/authorized_reestr.py
@@ -3,7 +3,17 @@ from rest_framework import serializers
 from main.models.authorized_reestr import AuthorizedReestr
 
 
+class ReestrSelectSerializer(serializers.Serializer):
+    area = serializers.ReadOnlyField()
+    number = serializers.ReadOnlyField()
+
+
 class AuthorizedReestrModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuthorizedReestr
         fields = '__all__'
+
+    def to_representation(self, instance):
+        self.fields['reestr'] = ReestrSelectSerializer(required=False)
+        self.fields['confirm_reestr'] = ReestrSelectSerializer(required=False)
+        return super().to_representation(instance)
